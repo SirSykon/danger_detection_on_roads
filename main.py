67561b400d32a35952b76b3f2f163db5f4836c44
Jorge García <sirsykon@gmail.com>
2021-11-26 19:01:51 +0100

    Initial testswith object_detection repo.

---
+++ main.py
@@ -0,0 +1,38 @@
+import yaml
+import sys
+import os
+
+import cv2
+
+# First, we obtain the configuration dictionary.
+yaml_file = open("./config.yml", 'r')
+config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+sys.path.insert(0, config["OBJECT_DETECTION_REPOSITORY_FOLDER"])
+
+import object_detection.utils.read_utils as read_utils
+from object_detection.object_detectors.object_detector import Object_Detector
+from object_detection.print_utils import print_utils
+
+output_folder = config["OUTPUT_FOLDER"]
+
+front_video_file_path = os.path.join(config["INPUT_FOLDER"], config["FRONT_VIDEO_NAME"])
+back_video_file_path = os.path.join(config["INPUT_FOLDER"], config["BACK_VIDEO_NAME"])
+
+front_video_gen = read_utils.generator_from_video(front_video_file_path)
+back_video_gen = read_utils.generator_from_video(back_video_file_path)
+
+object_detector = Object_Detector(config["BACKEND"], config["MODEL"], config["TRANSFORMATIONS"],config["MODEL_ORIGIN"])
+print(f"Loaded object detection model.")
+
+for index, (front_frame, back_frame) in enumerate(zip(front_video_gen, back_video_gen)):
+    rgb_front_frame = front_frame[:,:,[2,1,0]]
+    rgb_back_frame = back_frame[:,:,[2,1,0]]
+    output_front = object_detector.process_single_image(rgb_front_frame)
+    output_back = object_detector.process_single_image(rgb_back_frame)
+
+    front_drawn_image = print_utils.print_detections_on_image(output_front, front_frame)
+    back_drawn_image = print_utils.print_detections_on_image(output_back, back_frame)
+
+    cv2.imwrite(os.path.join(output_folder, f"front_{index}.jpg"), front_frame)
+    cv2.imwrite(os.path.join(output_folder, f"back_{index}.jpg"), back_frame)
