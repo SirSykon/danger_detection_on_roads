b74083c4e24bfdfbb520654b7d9cafcb78a3ef62
Jorge García <sirsykon@gmail.com>
2021-11-29 15:48:55 +0100

    Improved example.

--- main.py
+++ main.py
@@ -26,13 +26,30 @@ object_detector = Object_Detector(config["BACKEND"], config["MODEL"], config["TR
 print(f"Loaded object detection model.")
 
 for index, (front_frame, back_frame) in enumerate(zip(front_video_gen, back_video_gen)):
+    print(index)
     rgb_front_frame = front_frame[:,:,[2,1,0]]
     rgb_back_frame = back_frame[:,:,[2,1,0]]
     output_front = object_detector.process_single_image(rgb_front_frame)
+    print(f"Front image {index}")
+    for bbox, _class, confidence in zip(output_front[0], output_front[1], output_front[2]): 
+        print("Bounding Box")
+        print(bbox)
+        print("Class")
+        print(_class)
+        print("Confidence")
+        print(confidence)
     output_back = object_detector.process_single_image(rgb_back_frame)
+    print(f"Back image {index}")
+    for bbox, _class, confidence in zip(output_back[0], output_back[1], output_back[2]): 
+        print("Bounding Box")
+        print(bbox)
+        print("Class")
+        print(_class)
+        print("Confidence")
+        print(confidence)
 
     front_drawn_image = print_utils.print_detections_on_image(output_front, front_frame)
     back_drawn_image = print_utils.print_detections_on_image(output_back, back_frame)
 
-    cv2.imwrite(os.path.join(output_folder, f"front_{index}.jpg"), front_frame)
-    cv2.imwrite(os.path.join(output_folder, f"back_{index}.jpg"), back_frame)
+    cv2.imwrite(os.path.join(output_folder, f"front_{index}.jpg"), front_drawn_image)
+    cv2.imwrite(os.path.join(output_folder, f"back_{index}.jpg"), back_drawn_image)
